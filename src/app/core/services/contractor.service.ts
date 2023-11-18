80d3cd847a646c6b4d6a2329841b2c6d0bdc19ed
Samiya2000 <Sameera@technography.ca>
2023-11-18 20:19:39 +0530

    contractor changes

--- src/app/core/services/contractor.service.ts
+++ src/app/core/services/contractor.service.ts
@@ -1,9 +1,86 @@
 import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { HttpResponseModel } from '../models/http-response.model';
+import { ContractorRepository } from '../../data/repositories/contractor.repository';
+import {ContractorRequest, ContractorResponse} from '../models/contractor.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ContractorService {
+  constructor(private contractorRepo: ContractorRepository) {}
 
-  constructor() { }
+  // get all Contractor categories
+  getAllContractors(): Observable<ContractorResponse[]> {
+    return this.contractorRepo.getAllContractor().pipe(
+      map((res: any) => {
+        return res.data.map((item: any) => ({
+          id: item.id,
+          name: item.name,
+          contactNo: item.contactNo,
+          email: item.email,
+          address: item.address,
+          status: item.status,
+        })) as ContractorResponse[];
+      })
+    );
+  }
+
+  //  for save Contractor
+  saveContractor(
+    contractorData: ContractorRequest
+  ): Observable<{ msg: string; typ: number }> {
+    return this.contractorRepo.saveContractor(contractorData).pipe(
+      map((response: HttpResponseModel) => {
+        return {
+          msg: response.message,
+          typ: response.statusCode == 200 ? 1 : 2,
+        };
+      })
+    );
+  }
+
+  // for get Contractor by id
+  getContractorById(id: number): Observable<ContractorResponse> {
+    return this.contractorRepo.getContractorByID(id).pipe(
+      map((response: HttpResponseModel) => {
+        const responseData = response.data;
+        return {
+          id:responseData.id,
+          name:responseData.name,
+          address:responseData.address,
+          contactNo:responseData.contactNo,
+          email:responseData.email,
+          status:responseData.status
+        } as ContractorResponse;
+      })
+    );
+  }
+
+  // for update  Contractor details
+  updateContractor(
+    id: number,
+    ContractorData: ContractorRequest
+  ): Observable<{ msg: string; typ: number }> {
+    return this.contractorRepo.updateContractor(id, ContractorData).pipe(
+      map((response: HttpResponseModel) => {
+        return {
+          msg: response.message,
+          typ: response.statusCode == 200 ? 1 : 2,
+        };
+      })
+    );
+  }
+
+  // for delete Contractor details
+  deleteContractor(id: number): Observable<{ msg: string; typ: number }> {
+    return this.contractorRepo.deleteContractor(id).pipe(
+      map((response: HttpResponseModel) => {
+        return {
+          msg: response.message,
+          typ: response.statusCode == 200 ? 1 : 2,
+        };
+      })
+    );
+  }
 }
--- src/app/features/admin/pages/contractors/contractors.component.ts
+++ src/app/features/admin/pages/contractors/contractors.component.ts
@@ -9,18 +9,37 @@ import { AddAndUpdateContractorComponent } from '../../components/add-and-update
 import { TableComponent } from '../../../../shared/components/table/table.component';
 // models
 import { TableColumnModel } from '../../../../core/models/tableData.model';
-import {ContractorTableDataModel} from "../../../../core/models/contractor.model";
+import {
+  ContractorRequest,
+  ContractorResponse,
+  ContractorTableDataModel,
+} from '../../../../core/models/contractor.model';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { ContractorService } from '../../../../core/services/contractor.service';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-contractors',
   standalone: true,
-  imports: [CommonModule, AddAndUpdateContractorComponent, TableComponent],
+  imports: [
+    CommonModule,
+    AddAndUpdateContractorComponent,
+    TableComponent,
+    ConfirmDialogModule,
+    ToastModule,
+  ],
+  providers: [MessageService, ConfirmationService],
   templateUrl: './contractors.component.html',
   styleUrls: ['./contractors.component.css'],
 })
 export class ContractorsComponent {
   // for contractor details
   frmContractor!: FormGroup;
+  // for store all contractor details
+  allContractors!: ContractorResponse[];
+  // for change form state to save or update
+  isFormSaveState: boolean = true;
   // for contractor table schema
   contractorTableSchema: TableColumnModel[] = [
     { field: 'contractorName', header: 'Contractor Name' },
@@ -29,22 +48,38 @@ export class ContractorsComponent {
     { field: 'address', header: 'Address' },
   ];
   // for contractor table data
-  contractorTableData:ContractorTableDataModel[]=[
-    {id:1,contractorName:'Sameera',address:'No 04,kandy',contactNo:'0785485682',emailAddress:'sameera@gmail.com'},
-    {id:1,contractorName:'Sameera',address:'No 04,kandy',contactNo:'0785485682',emailAddress:'sameera@gmail.com'},
-    {id:1,contractorName:'Sameera',address:'No 04,kandy',contactNo:'0785485682',emailAddress:'sameera@gmail.com'},
-    {id:1,contractorName:'Sameera',address:'No 04,kandy',contactNo:'0785485682',emailAddress:'sameera@gmail.com'},
-    {id:1,contractorName:'Sameera',address:'No 04,kandy',contactNo:'0785485682',emailAddress:'sameera@gmail.com'},
-    {id:1,contractorName:'Sameera',address:'No 04,kandy',contactNo:'0785485682',emailAddress:'sameera@gmail.com'},
-  ]
+  contractorTableData: ContractorTableDataModel[] = [];
 
-  constructor(private frmBuilder: FormBuilder) {
+  constructor(
+    private frmBuilder: FormBuilder,
+    private messageService: MessageService,
+    private confirmationService: ConfirmationService,
+    private contractorService: ContractorService
+  ) {
     this.initializeForms();
+    this.getAllContractors();
+  }
+
+  // for get all contractors
+  getAllContractors(): void {
+    this.contractorService.getAllContractors().subscribe((res) => {
+      this.allContractors = res;
+      this.contractorTableData = this.allContractors
+        .filter((item) => item.status > 0)
+        .map((item) => ({
+          id: item.id,
+          address: item.address,
+          contractorName: item.name,
+          contactNo: item.contactNo,
+          emailAddress: item.email,
+        }));
+    });
   }
 
   // for initialize forms
   initializeForms(): void {
     this.frmContractor = this.frmBuilder.group({
+      id: ['', []],
       contractorName: ['', [Validators.required]],
       contactNo: ['', [Validators.required]],
       emailAddress: ['', [Validators.required]],
@@ -56,7 +91,95 @@ export class ContractorsComponent {
   saveContractor(): void {
     this.frmContractor.markAllAsTouched();
     if (this.frmContractor.valid) {
-      console.log(this.frmContractor.getRawValue());
+      let contractorData: ContractorRequest = {
+        name: this.frmContractor.get('contractorName')?.value,
+        contactNo: this.frmContractor.get('contactNo')?.value,
+        address: this.frmContractor.get('address')?.value,
+        email: this.frmContractor.get('emailAddress')?.value,
+        status: 1,
+      };
+      this.contractorService.saveContractor(contractorData).subscribe((res) => {
+        this.messageService.add({
+          severity: res.typ === 1 ? 'success' : 'error',
+          summary: res.typ === 1 ? 'Success' : 'Error',
+          detail: res.msg,
+        });
+        this.frmContractor.reset();
+        if (res.typ === 1) {
+          window.location.reload();
+        }
+      });
     }
   }
+
+  // for set selected element to update
+  selectContractorToUpdate(id: number) {
+    this.isFormSaveState = false;
+    this.contractorService.getContractorById(id).subscribe((res) => {
+      this.frmContractor.get('id')?.setValue(res.id);
+      this.frmContractor.get('contractorName')?.setValue(res.name);
+      this.frmContractor.get('contactNo')?.setValue(res.contactNo);
+      this.frmContractor.get('address')?.setValue(res.address);
+      this.frmContractor.get('emailAddress')?.setValue(res.email);
+    });
+  }
+
+  //update contractor
+  updateContractor() {
+    this.confirmationService.confirm({
+      message: 'Do you want to update this record?',
+      header: 'Delete Confirmation',
+      icon: 'pi pi-info-circle',
+      accept: () => {
+        let contractorData: ContractorRequest = {
+          name: this.frmContractor.get('contractorName')?.value,
+          contactNo: this.frmContractor.get('contactNo')?.value,
+          address: this.frmContractor.get('address')?.value,
+          email: this.frmContractor.get('emailAddress')?.value,
+          status: 1,
+        };
+
+        this.contractorService
+          .updateContractor(this.frmContractor.get('id')?.value, contractorData)
+          .subscribe((res) => {
+            this.messageService.add({
+              severity: res.typ === 1 ? 'success' : 'error',
+              summary: res.typ === 1 ? 'Success' : 'Error',
+              detail: res.msg,
+            });
+            this.frmContractor.reset();
+            if (res.typ === 1) {
+              window.location.reload();
+            }
+          });
+      },
+      reject: () => {},
+    });
+  }
+
+  deleteContractor(id: number) {
+    this.confirmationService.confirm({
+      message: 'Do you want to delete this record?',
+      header: 'Delete Confirmation',
+      icon: 'pi pi-info-circle',
+      accept: () => {
+        this.contractorService
+          .deleteContractor(id)
+          .subscribe((res) => {
+            this.messageService.add({
+              severity: res.typ === 1 ? 'success' : 'error',
+              summary: res.typ === 1 ? 'Success' : 'Error',
+              detail: res.msg,
+            });
+            this.frmContractor.reset();
+            if (res.typ === 1) {
+              window.location.reload();
+            }
+          });
+      },
+      reject: () => {},
+    });
+  }
+
+  protected readonly event = event;
 }
